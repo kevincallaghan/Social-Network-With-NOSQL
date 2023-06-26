1c9d503746b20df10e488402ea6428de2806af2e
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-26 15:03:27 -0500

    starting routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+//example from class mini project
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Wrong route!'));
+
+module.exports = router;
