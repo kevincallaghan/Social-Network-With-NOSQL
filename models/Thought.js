11cb34b7ee688d3105ff4f60fd1ba325f2e4176b
kevincallaghan <kevincallaghan5@yahoo.com>
2023-06-26 12:50:06 -0500

    working on Thought.js and User.js

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxLength: 280,
+      minLength: 1,
+    },
+    username: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    reactions: 
+    //TODO insert array of reactions
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toLocaleString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Reaction = require('./Reaction');
+const Thought =require('./Thought');
+
+
+
+module.exports = {User,Reaction,Thought};
